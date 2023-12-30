17b5790510c184fe137191269bc8f7b0e4285839
michiel-celis-91 <michiel.celis@outlook.com>
2023-12-30 12:54:19 +0100

    placed planet moon and sun, propers scaling and windows resize limits

--- celis-michiel-c-sherp/MainWindow.xaml.cs
+++ celis-michiel-c-sherp/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Globalization;
 
 namespace celis_michiel_c_sherp
 {
@@ -23,6 +24,36 @@ namespace celis_michiel_c_sherp
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+		private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+		{
+			Window window = sender as Window;
+			if (window != null)
+			{
+				double aspectRatio = window.Width / window.Height;
+				if (aspectRatio < 4.0 / 3.0) // If the aspect ratio is less than 4:3
+				{
+					window.Width = window.Height * 4.0 / 3.0; // Adjust the width
+				}
+				else if (aspectRatio > 16.0 / 9.0) // If the aspect ratio is greater than 16:9
+				{
+					window.Height = window.Width * 9.0 / 16.0; // Adjust the height
+				}
+			}
+		}
+	}
+	public class ViewportPercentageConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			double viewportSize = System.Convert.ToDouble(value);
+			double percentage = System.Convert.ToDouble(parameter);
+			return viewportSize * (percentage / 100);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
